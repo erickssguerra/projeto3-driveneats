bf449cf412446213e40150fd25e59888b32fdcb9
erickssguerra <erickssguerra@gmail.com>
2022-08-15 15:53:31 -0300

    Versão final + Bônus 1

--- script2.js
+++ script2.js
@@ -9,18 +9,18 @@ let dessertPrice = 0;
 
 function pickDish(parameter, preco) {
     const selectedDish = document.querySelector(".dish .box-selected");
-    
+
 
 
     if (selectedDish !== null) {
         selectedDish.classList.remove("box-selected");
-    } 
+    }
     parameter.classList.add("box-selected");
     dishName = document.querySelector(".dish .box-selected h3").innerHTML
     dishPrice = preco
-    
+
     allowButton();
-   
+
 }
 
 function pickDrink(parameter, preco) {
@@ -29,9 +29,9 @@ function pickDrink(parameter, preco) {
 
     if (selectedDrink !== null) {
         selectedDrink.classList.remove("box-selected");
-    } 
+    }
     parameter.classList.add("box-selected");
-    
+
     drinkName = document.querySelector(".drink .box-selected h3").innerHTML;
     drinkPrice = preco;
 
@@ -51,37 +51,45 @@ function pickDessert(parameter, preco) {
 
     dessertName = document.querySelector(".dessert .box-selected h3").innerHTML;
     dessertPrice = preco;
-    
+
     allowButton();
 }
 
-function allowButton(){
+function allowButton() {
 
- 
+
     if (dishName !== undefined && drinkName !== undefined && dessertName !== undefined) {
         let buttonVar = document.querySelector("button");
         buttonVar.classList.add("button-available");
         buttonVar.innerHTML = `Fechar o pedido`;
-        
+
     }
 
 }
 
-function whatsapp () {
-    
+function whatsapp() {
+
     if (dishName !== undefined && drinkName !== undefined && dessertName !== undefined) {
-        let total = dishPrice + drinkPrice + dessertPrice
-        
-        let whatsappMessage = 
-            
+        let total = dishPrice + drinkPrice + dessertPrice;
+        let name = prompt('A entrega será em qual nome?');
+        let address = prompt('A entrega será em qual endereço?');
+        let whatsappMessage =
+
             `Olá, gostaria de fazer o pedido:
             - Prato: ${dishName}
             - Bebida: ${drinkName}
             - Sobremesa: ${dessertName}
-            Total: R$ ${total.toFixed(2)}`
-       
+            Total: R$ ${total.toFixed(2)}
+            
+            Nome: ${name}
+            Endereço: ${address}`
+
         let url = "https://wa.me/5584999578788?text=" + encodeURIComponent(whatsappMessage);
         window.open(url)
 
     }
 }
+/*
+function confirm() {
+
+}*/
